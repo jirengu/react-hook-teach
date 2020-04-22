c8228ba1c93a62629483c8321c3bf26fd4b6fef9
sun-shadow <sun-shadow@gmail.com>
2020-04-22 15:42:18 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+
+function Counter() {
   const [count, setCount] = useState(0)
+  const [age, setAge] = useState(20)
+
+  // 相当于 componentDidMount 和 componentDidUpdate:
+  /*
+  useEffect(() => {
+    console.log(`You clicked ${count} times`)
+  }) //监控所有
+  */
+
+  /*
+  useEffect(() => {
+    console.log('挂载和更新时执行')
+    return () => {
+      console.log('执行清除——更新和卸载时执行')
+    }
+  }, [count])  //只监控count
+  */
+
+  useEffect(() => {
+    console.log('挂载和更新时执行')
+    return () => {
+      console.log('执行清除——更新和卸载时执行')
+    }
+  }, [])   //都不监控，只在第一次挂载时执行一次
+
 
   return (
     <div className="App">
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}> Click me </button>
+      <button onClick={() => setCount(count + 1)}> Add count</button>
+      <p>You are {age} years old!</p>
+      <button onClick={() => setAge(age + 1)}> Add age </button>     
     </div>
   );
+}
+
+
+function App() {
+  const [isShow, setIsShow] = useState(true)
+
+  return (
+    <div className="App">
+      <button onClick={() => setIsShow(!isShow)}>显示/隐藏Counter</button>
+      {
+        isShow && <Counter/>
+      }
+    </div>
+  )
 }
 
 export default App;
